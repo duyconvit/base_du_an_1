6e467c4213bf40119d6aaaa427d59ed116a14824
duyconvit <duynvph49893@gmail.com>
2025-03-20 00:04:27 +0700

    Duy đẩy Db test

--- commons/env.php
+++ commons/env.php
@@ -3,7 +3,7 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'   , 'http://localhost/base_du_an_1/');
+define('BASE_URL'   , 'http://localhost/base_du_an_1/'); // Đường dẫn tới client
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
